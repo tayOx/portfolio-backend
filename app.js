87de7f2a8a8df27f1f6d4f4a6cd33821146861d0
tayo <you@example.com>
2022-04-16 18:49:52 -0300

    qr code backend v1.0

--- app.js
+++ app.js
@@ -32,10 +32,32 @@ app.all('/ip', (req, res) => {
     axios({
 
         method: "get",
-        url: "http://ip-api.com/json/"+query+"?fields=17982485",
+        url: "http://ip-api.com/json/"+query+"?fields=1209365",
         responseType: "json"
 
     }).then((data) => res.send(data.data));
 
 });
 
+app.post('/genqr', (req, res) => {
+
+    var type = req.body.type;
+    var value = req.body.value;
+    console.log(type, value)
+
+    const options = {
+
+        method: "GET",
+        url: "https://codzz-qr-cods.p.rapidapi.com/getQrcode",
+        params: {type: type, value: value},
+        headers: {
+
+            'X-RapidAPI-Host': 'codzz-qr-cods.p.rapidapi.com',
+            'X-RapidAPI-Key': 'a578b8ba3emsh56315883d4ff6bap13fca3jsn1ad3c1c28488'
+
+        }
+    };
+
+    axios.request(options).then((data) => res.send(data.data));
+
+});
